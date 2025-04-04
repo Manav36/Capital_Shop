b21ee3e851402423fe08ba12a92a6eb4b467df1a
unknown <manavnp11@gmail.com>
2025-04-04 12:06:40 +0545

    commit message

---
+++ script.js
@@ -0,0 +1,16 @@
+// Initialize Swiper
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
